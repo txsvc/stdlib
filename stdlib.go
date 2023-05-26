411b3cf43b78c54ec12a7dd86148ad28fe08de8d
Michael Kuehl <hello@txs.vc>
2023-05-26 08:54:32 +0200

    added something about logging

--- stdlib.go
+++ stdlib.go
@@ -1,5 +1,15 @@
 package stdlib
 
+import (
+	"strings"
+
+	"github.com/rs/zerolog"
+)
+
+const (
+	LOG_LEVEL = "LOG_LEVEL"
+)
+
 // Placeholder is a placeholder object that can be used globally.
 var Placeholder PlaceholderType
 
@@ -9,3 +19,21 @@ type (
 	// PlaceholderType represents a placeholder type.
 	PlaceholderType = struct{}
 )
+
+func SetLogLevel() {
+	// setup logging
+	log_level := strings.ToLower(GetString(LOG_LEVEL, ""))
+	if log_level == "trace" {
+		zerolog.SetGlobalLevel(zerolog.TraceLevel)
+	} else if log_level == "debug" {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	} else if log_level == "info" {
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	} else if log_level == "warn" {
+		zerolog.SetGlobalLevel(zerolog.WarnLevel)
+	} else if log_level == "error" {
+		zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+	} else {
+		zerolog.SetGlobalLevel(zerolog.Disabled)
+	}
+}
